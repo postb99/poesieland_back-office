1fd93611e58c370712abf10e07bf89d68c872ea7
Barbara Post <post.barbara@gmail.com>
2026-01-06 22:29:37 +0100

    Fiw which tests should not run when real data

--- src/Tests/Charts/ChartDataFileHelperTest.cs
+++ src/Tests/Charts/ChartDataFileHelperTest.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace Tests.Charts;
 
-public class ChartDataFileHelperTest(WithRealDataFixture fixture, ITestOutputHelper testOutputHelper): IClassFixture<WithRealDataFixture>
+public class ChartDataFileHelperTest
 {
     /// <summary>
     /// - xAxisLabelsForCallback: set
@@ -59,20 +59,5 @@ public class ChartDataFileHelperTest(WithRealDataFixture fixture, ITestOutputHel
 
         formattedString.Count(x => x == '{').ShouldBe(formattedString.Count(x => x == '}'));
         formattedString.ShouldBe("scales: { x: { ticks: { stepSize: 1, autoSkip: false, callback: function(value, index, ticks) { return ['A','B','D'][index]; } }, title: {display:true, text:'X'} }, y: { ticks: { stepSize: 1, autoSkip: false }, title: {display:true, text:'Y'} } }");
-    }
-    
-    [Fact]
-    [Trait("UnitTest", "Quality")]
-    public void ShouldCorrectlyComputeVerseLengthDataDict()
-    {
-        var data = fixture.Data;
-        var dataDict = ChartDataFileHelper.FillMetricDataDict(data, out var _);
-        testOutputHelper.WriteLine(
-            $"Last non-empty season poem count: {data.Seasons.Last(x => x.Poems.Count > 0).Poems.Count}");
-        testOutputHelper.WriteLine(
-            $"Computed values for last season: {string.Join('-', dataDict.Values.Select(x => x.Last()))}");
-        var sum = dataDict.Values.Sum(x => x.Last());
-        testOutputHelper.WriteLine($"Computed values sum: {sum}");
-        sum.ShouldBeInRange(49m, 59m);
     }
 }
